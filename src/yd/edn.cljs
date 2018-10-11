2ae0618b551ec52e572dc7c880d670c11f86eeef
Brian Hurlow <brian@brianhurlow.com>
2018-10-11 11:47:44 -0400

    call read-string on values in encode

--- src/yd/edn.cljs
+++ src/yd/edn.cljs
@@ -4,33 +4,48 @@
             [cljs.tools.reader :refer (read-string)]))
 
 
-(defn interpret-kw [k-str]
-  (if (and (string? k-str)
-           (.startsWith k-str ":"))
-    (-> k-str
-        (replace ":" "")
-        (keyword))
-    k-str))
+(defn keylike? [k-str]
+  (and (string? k-str)
+       (.startsWith k-str ":")))
+
+
+(defn keywordize [k-str]
+  (-> k-str
+      (replace ":" "")
+      (keyword)))
+
+
+(defn interpret-key [k-str]
+  (cond
+    (keylike? k-str) (keywordize k-str)
+    true k-str))
+
+
+(defn interpret-val [value]
+  (if (string? value)
+    (read-string value)
+    value))
 
 
 (defn print-kw [x]
   (if (keyword? x) (str x) x))
 
 
-(defn walk [m tag-fn]
-  (let [f (fn [[k v]] [(tag-fn k) (tag-fn v)])]
-    (postwalk (fn [x] (if (map? x) (into {} (map f x)) (tag-fn x))) m)))
+(defn walk [m key-fn val-fn]
+  (let [f (fn [[k v]] [(key-fn k) (val-fn v)])]
+    (postwalk (fn [x] (if (map? x) (into {} (map f x)) (key-fn x))) m)))
 
 
 (defn encode [js-data keywordize?]
   (cond-> (js->clj js-data) 
-      keywordize? (walk interpret-kw) 
+      keywordize? (walk interpret-key interpret-val) 
+      (not keywordize?) (walk identity interpret-val)
       true pr-str))
 
 
 (defn decode [edn-str keywordize?]
   (cond-> (read-string edn-str)
-    keywordize? (walk print-kw)
+    keywordize? (walk print-kw identity)
     true clj->js))
     
 
