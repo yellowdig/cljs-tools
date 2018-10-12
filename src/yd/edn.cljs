0cb69981a8b9c1123da02db8f06db10c3da30077
Brian Hurlow <brian@brianhurlow.com>
2018-10-12 14:34:04 -0400

    try/catch read-string during encode

--- src/yd/edn.cljs
+++ src/yd/edn.cljs
@@ -21,10 +21,16 @@
     true k-str))
 
 
+(defn try-read [value]
+  (try
+    (read-string value)
+    (catch js/Error e nil)))
+
+
 ;; 1. if value is a url, treat it as a string literal
 ;; 2. js cannot distinguish between symbols vs strings, thus don't allow encoding of symbols
 (defn interpret-val [value]
-  (let [read-val (read-string value)]
+  (let [read-val (try-read value)]
     (cond
       (string? value)
       (cond
