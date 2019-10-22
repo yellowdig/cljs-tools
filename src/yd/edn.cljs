12fbd58254e32480ccd87389a9ca9619e01408e0
Xander Goldman <xander@yellowdig.com>
2019-10-22 15:41:27 -0400

    Updated binding call

--- src/yd/edn.cljs
+++ src/yd/edn.cljs
@@ -3,8 +3,6 @@
             [clojure.string :refer (replace includes?)]
             [cljs.reader :refer (read-string)]))
 
-(binding [*data-readers* {'inst cljs.tagged-literals/read-inst}])
-
 (defn keylike? [k-str]
   (and (string? k-str)
        (.startsWith k-str ":")))
@@ -23,7 +21,8 @@
 
 
 (defn read-data [data]
-  (read-string data))
+  (binding [*data-readers* {'inst cljs.tagged-literals/read-inst}]
+    (read-string data)))
 
 
 (defn try-read [value]
