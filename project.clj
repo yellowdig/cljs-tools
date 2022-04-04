38409b6efb1cc2799b3d101cc6c67aef56e43875
Brian Hurlow <brian@brianhurlow.com>
2022-04-04 13:39:24 -0400

    bump deps, add reader tag to read #datom values as vectors

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject cljs-tools "0.0.1"
+(defproject cljs-tools "0.1.0"
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.10.238"]]
+                 [org.clojure/clojurescript "1.11.4"]]
   :plugins [[lein-cljsbuild "1.1.5"]]
   :cljsbuild {:builds [{:id "release"
                         :source-paths ["src"]
--- src/yd/edn.cljs
+++ src/yd/edn.cljs
@@ -1,39 +1,35 @@
 (ns yd.edn
-  (:require [clojure.walk :refer (postwalk)]
-            [clojure.string :refer (replace includes?)]
-            [cljs.reader :refer (read-string)]))
-
+  (:require
+   [cljs.reader :refer (read-string)]
+   [clojure.string :refer (replace includes?)]
+   [clojure.walk :refer (postwalk)]))
 
 (defn keylike? [k-str]
   (and (string? k-str)
        (.startsWith k-str ":")))
-
 
 (defn keywordize [k-str]
   (-> k-str
       (replace ":" "")
       (keyword)))
 
-
 (defn interpret-key [k-str]
   (cond
     (keylike? k-str) (keywordize k-str)
     true k-str))
 
-
 (defn read-data [data]
-  (read-string data))
-
+  (read-string
+   {:readers {'datom (fn [x] x)}}
+   data))
 
 (defn try-read [value]
   (try
     (read-data value)
     (catch js/Error e nil)))
 
-
 (defn is-numeric? [num-str]
   (number? (js/Number num-str)))
-
 
 ;; 1. if value is a url, treat it as a string literal
 ;; 2. js cannot distinguish between symbols vs strings, thus don't allow encoding of symbols
@@ -50,31 +46,25 @@
         :else read-val)
       :else value)))
 
-
 (defn print-kw [x]
   (if (keyword? x) (str x) x))
 
-
 (defn walk [m key-fn val-fn]
   (let [f (fn [[k v]] [(key-fn k) (val-fn v)])]
-    (postwalk 
-      (fn [x] 
-        (if (map? x) 
-          (into {} (map f x)) 
-          (key-fn x))) 
-      m)))
-
+    (postwalk
+     (fn [x]
+       (if (map? x)
+         (into {} (map f x))
+         (key-fn x)))
+     m)))
 
 (defn encode [js-data keywordize?]
-  (cond-> (js->clj js-data) 
-      keywordize? (walk interpret-key interpret-val) 
-      (not keywordize?) (walk identity interpret-val)
-      true pr-str))
-
+  (cond-> (js->clj js-data)
+    keywordize? (walk interpret-key interpret-val)
+    (not keywordize?) (walk identity interpret-val)
+    true pr-str))
 
 (defn decode [edn-str keywordize?]
   (cond-> (read-data edn-str)
     keywordize? (walk print-kw identity)
     true clj->js))
-    
-
